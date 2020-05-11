c11a618f720ad4488316b27dee4639e0637d3915
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2020-05-11 17:33:59 +0200

    dodanie pliku

---
+++ aplikacja/passwd.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+
+<html lang="pl">
+<head>
+	
+	<title>E-Dziennik</title>
+	<META http-equiv="content-type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="styleLogin.css" type="text/css" />
+	<link rel="Shortcut icon" href="favicon.ico" />
+	
+	<meta name="description" content="opis w google"/>
+	<meta name="keywords" content="słowa po których google szuka"/>
+
+	<meta http-equiv="X-UA_Compatible" content="IE=edge,chrome=1" />
+	<meta name="author" content="Kowalski, Mielniczek, Pająk" />
+
+</head>
+
+
+<body>
+	
+	<div id="container">
+	
+		<div id="logo">
+		
+			<h1>Reset hasła</h1>
+		
+		</div>
+		
+		
+		<div id="tresc">
+		
+		Adres e-mail:<br/>
+		<input type="text" name="stare"><br/>
+
+		<button type="button">Reset hasła</button>
+		
+		
+		</div>
+
+		
+		<div id="footer">
+		e-dziennik
+		</div>
+	
+	</div>
+	
+</body>
+
+
+</html>
